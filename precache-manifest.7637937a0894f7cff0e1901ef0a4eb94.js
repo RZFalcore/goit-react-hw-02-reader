ae10f4ca6008db0a48e288ad325bc15d90013985
RZFalcore <box.rz.box@gmail.com>
2019-07-19 15:47:21 +0300

    Updates

---
+++ precache-manifest.7637937a0894f7cff0e1901ef0a4eb94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a19c229bc410ad558662df9e38bacc56",
+    "url": "/RZFalcore/goit-react-hw-02-reader/index.html"
+  },
+  {
+    "revision": "4214f6959deb81c828e7",
+    "url": "/RZFalcore/goit-react-hw-02-reader/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/RZFalcore/goit-react-hw-02-reader/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "4214f6959deb81c828e7",
+    "url": "/RZFalcore/goit-react-hw-02-reader/static/js/main.0b3d5e50.chunk.js"
+  },
+  {
+    "revision": "9294870bc481cd7a4781",
+    "url": "/RZFalcore/goit-react-hw-02-reader/static/js/runtime~main.5c54bf53.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/RZFalcore/goit-react-hw-02-reader/static/media/logo.5d5d9eef.svg"
+  }
+]);
